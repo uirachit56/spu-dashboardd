44ae1c7745cf916fc66ebb32062271dc8fafae14
Rachit Mevada <uirachit56@gmail.com>
2026-07-31 12:10:23 +0530

    Final update

--- sw.js
+++ sw.js
@@ -1,22 +1,65 @@
-const CACHE_NAME = "spu-dashboard-v2";
+// const CACHE_NAME = "spu-dashboard-v10";
+
+// const urlsToCache = [
+//   "/",
+//   "/index.html",
+//   "/style.css"
+// ];
+
+// self.addEventListener("install", (event) => {
+//   event.waitUntil(
+//     caches.open(CACHE_NAME).then((cache) => {
+//       return cache.addAll(urlsToCache);
+//     })
+//   );
+// });
+
+// self.addEventListener("fetch", (event) => {
+//   event.respondWith(
+//     caches.match(event.request).then((response) => {
+//       return response || fetch(event.request);
+//     })
+//   );
+// });
+
+const CACHE_NAME = "spu-dashboard-v10";
 
 const urlsToCache = [
   "/",
   "/index.html",
-  "/style.css"
+  "/transport.html",
+  "/style.css",
+  "/transport.css"
 ];
 
-self.addEventListener("install", (event) => {
+self.addEventListener("install", event => {
+  self.skipWaiting();
+
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener("fetch", (event) => {
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
+    caches.match(event.request).then(response => {
       return response || fetch(event.request);
     })
   );
